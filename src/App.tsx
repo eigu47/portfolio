9a454800775cd6b2f55b1eb312143381e0cf792d
Eiguchi Pablo <pablo.eiguchi@gmail.com>
2023-03-29 10:46:29 +0900

    interactive nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import { Suspense } from "react";
-
-import { Canvas } from "@react-three/fiber";
-
-import Keyboard from "~/components/Keyboard";
+import Hero from "~/components/Hero";
 import Layout from "~/components/Layout";
 import Navbar from "~/components/Navbar";
 
@@ -10,38 +6,9 @@ export default function App() {
   return (
     <>
       <Navbar />
-      <main className="h-screen">
-        <div className="absolute bottom-0 h-[50vh] w-full">
-          <Scene>
-            <Keyboard />
-          </Scene>
-        </div>
-      </main>
+      <Hero />
       <div className="h-screen" />
       <Layout />
     </>
-  );
-}
-
-function Scene({
-  children,
-  cameraPos = [0, 0, 4],
-}: {
-  children: React.ReactNode;
-  cameraPos?: [number, number, number];
-}) {
-  return (
-    <Canvas shadows camera={{ position: cameraPos }}>
-      {/* <color attach="background" args={["slategray"]} /> */}
-      <ambientLight intensity={0.5} />
-      <spotLight
-        castShadow
-        intensity={0.5}
-        position={[10, 15, 10]}
-        angle={0.2}
-        penumbra={1}
-      />
-      <Suspense fallback={null}>{children}</Suspense>
-    </Canvas>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,42 @@
 export default function Navbar() {
-  return <nav>Navbar</nav>;
+  return (
+    <nav className="sticky top-0 z-10 flex h-20 w-full items-center bg-slate-950/50 shadow-md backdrop-blur">
+      <ul className="container mx-auto flex gap-10 text-slate-300">
+        {NAVITEMS.map(({ name, href, className = "" }) => (
+          <li key={name} className={className}>
+            <a
+              className="relative p-3 transition duration-300 after:absolute after:bottom-1 after:right-1/2 after:h-[2px] after:w-4/6 after:origin-center after:translate-x-1/2 after:scale-x-0 after:bg-emerald-400 after:transition hover:text-white hover:after:scale-x-100"
+              href={href}
+            >
+              {name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
+
+const NAVITEMS = [
+  {
+    name: "Home",
+    href: "#",
+    className: "grow",
+  },
+  {
+    name: "About",
+    href: "#about",
+  },
+  {
+    name: "Projects",
+    href: "#projects",
+  },
+  {
+    name: "Contact",
+    href: "#contact",
+  },
+  {
+    name: "Resume",
+    href: "#resume",
+  },
+];
